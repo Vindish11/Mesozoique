7d40a572437f1627a7cca1f04bbf629e3f8256af
Vindish11 <88803712+Vindish11@users.noreply.github.com>
2021-08-12 00:51:03 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const path = require('path') // on ajoute la librairie path
 
 const client = new CommandoClient({
 	commandPrefix: '-', // Préfixe des commandes (ex: ?help)
-	owner: process.env.BOT_OWNER_ID, // ID de l'owner du bot, peut également être un tableau d'id pour plusieurs owners, ex: ['ID1', 'ID2']
+	owner: Vindish, // ID de l'owner du bot, peut également être un tableau d'id pour plusieurs owners, ex: ['ID1', 'ID2']
         disableMentions: 'none' // Désactive, par sécurité, l'utilisation du everyone par le bot
 });
 
 client.registry
     .registerDefaultTypes()
     .registerGroups([])
-    .registerCommandsIn(path.join(__dirname, 'commandes'))
+    .registerCommandsIn(path.join(__dirname, 'Commandes'))
 ;
 
 client.once('ready', () => {
