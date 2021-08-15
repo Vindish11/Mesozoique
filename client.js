5c6011d77966ae88a59bf3260833a9bb85d635e2
Vindish11 <88803712+Vindish11@users.noreply.github.com>
2021-08-15 17:21:00 +0200

    Delete client.js

--- client.js
+++
@@ -1,8 +0,0 @@
-const { CommandoClient } = require('discord.js-commando')
-
-module.exports = class BotClient extends CommandoClient {
-    constructor(options) {
-        super(options);
-
-    }
-};
