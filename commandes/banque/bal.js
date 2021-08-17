b1765e2ad7237533acbd77758cbbc5f72479617f
Vindish11 <88803712+Vindish11@users.noreply.github.com>
2021-08-17 10:46:52 +0200

    Update bal.js

--- commandes/banque/bal.js
+++ commandes/banque/bal.js
@@ -1,4 +1,4 @@
-client.on("message", msg => {
-  if (msg.content === "-bal") {
-     msg.channel.send('Your current balance is ' + db.get(msg.author.id + '.bal') + '.');
-  }
+//client.on("message", msg => {
+//  if (msg.content === "-bal") {
+//     msg.channel.send('Your current balance is ' + db.get(msg.author.id + '.bal') + '.');
+//  }
