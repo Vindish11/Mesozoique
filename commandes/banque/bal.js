2bc0f89f57425dc9cb4de367451b1c96409db9df
Vindish11 <88803712+Vindish11@users.noreply.github.com>
2021-08-18 16:59:10 +0200

    Update bal.js

--- commandes/banque/bal.js
+++ commandes/banque/bal.js
@@ -1,4 +1,22 @@
-//client.on("message", msg => {
-//  if (msg.content === "-bal") {
-//     msg.channel.send('Your current balance is ' + db.get(msg.author.id + '.bal') + '.');
-//  }
+const { Command } = require('discord.js-commando');
+
+module.exports = class HelloCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'hello',
+			memberName: 'hello',
+			group: 'divers',
+			aliases: ['bonjour', 'hi'],
+			description: 'Replies with a hello message.',
+	                guildOnly: false,
+	                throttling: {
+	                        usages: 2,
+	                        duration: 10,
+	                },
+		});
+	}
+
+	async run(msg) {
+            msg.say(`Bonjour, je suis ${this.client.user.tag} (\`${this.client.user.id}\`)`);
+	}
+};
